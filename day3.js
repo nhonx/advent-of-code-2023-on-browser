9f4c769780eaff5f6c08bc9c92b434a665ac5530
Nhon Dinh <nhonx91@gmail.com>
2023-12-06 12:29:46 +0700

    Day 3 - part 2

--- day3.js
+++ day3.js
@@ -34,3 +34,70 @@ const part1 = () => {
     return sum;
 }
 console.log("Part 1", part1());
+
+const part2 = () => {
+    b = document.getElementsByTagName("pre")[0].innerText.split("\n");
+    sum = 0;
+    function getNumber(a, i) {
+        const isNum = (c) => c && !isNaN(parseInt(c))
+        let rs = 0;
+        for (var j = 0; j < a.length; j++) {
+            if (a[j] == '*') {
+                const muls = []
+                if (i > 0) {
+                    let pos = b[i - 1].split("").map((x, idx) => isNum(x) ? idx : -1).filter(x => x >= 0 && x >= j - 1 && x <= j + 1)
+                    const nums = [...b[i - 1].matchAll(/(\d+)/g)]
+                    pos.forEach(p => {
+                        nums.forEach(n => {
+                            if (p >= n.index && p <= n.index + n[0].length - 1) {
+                                if (muls.filter(x => x[0] === `${i - 1}${n.index}`).length == 0) {
+                                    muls.push([`${i - 1}${n.index}`, parseInt(n[0])]);
+                                }
+                            }
+                        })
+                    })
+
+                }
+                if (i < b.length - 1) {
+                    let pos = b[i + 1].split("").map((x, idx) => isNum(x) ? idx : -1).filter(x => x >= 0 && x >= j - 1 && x <= j + 1)
+                    const nums = [...b[i + 1].matchAll(/(\d+)/g)]
+                    pos.forEach(p => {
+                        nums.forEach(n => {
+                            if (p >= n.index && p <= n.index + n[0].length - 1) {
+                                if (muls.filter(x => x[0] === `${i + 1}${n.index}`).length == 0) {
+                                    muls.push([`${i + 1}${n.index}`, parseInt(n[0])]);
+                                }
+                            }
+                        })
+                    })
+                }
+                if (isNum(a[j - 1])) {
+                    const nums = [...b[i].matchAll(/(\d+)/g)]
+                    nums.forEach(n => {
+                        if (j - 1 >= n.index && j - 1 <= n.index + n[0].length - 1) {
+                            muls.push([`${i}${n.index}`, parseInt(n[0])]);
+                        }
+                    })
+                }
+                if (isNum(a[j + 1])) {
+                    const nums = [...b[i].matchAll(/(\d+)/g)]
+                    nums.forEach(n => {
+                        if (j + 1 >= n.index && j + 1 <= n.index + n[0].length - 1) {
+                            muls.push([`${i}${n.index}`, parseInt(n[0])]);
+                        }
+                    })
+                }
+                if (muls.length === 2) {
+                    rs += muls[0][1] * muls[1][1]
+                }
+            }
+        }
+        return rs;
+    }
+    b.forEach((x, idx) => {
+        let k = getNumber(x, idx);
+        sum = sum + k;
+    });
+    return sum;
+}
+console.log("Part 2", part2());
